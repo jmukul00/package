297ecbd96824010b96672f5b5e7408c90c7f7f5c
Mukul Jain <mukul.jain@sunarctechnologies.com>
2022-12-12 18:17:48 +0530

    added provider for sign in mobile

--- lib/src/sign_in/sign_in_mobile.dart
+++ lib/src/sign_in/sign_in_mobile.dart
@@ -7,7 +7,10 @@ import '../auth/auth_cubit.dart';
 import '../auth/auth_state.dart';
 
 class SignInMobile extends StatelessWidget {
-  const SignInMobile({Key? key}) : super(key: key);
+  String name ;
+  SignInMobile({Key? key, this.name = ""}) : super(key: key);
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -17,78 +20,84 @@ class SignInMobile extends StatelessWidget {
         child: MaterialApp(
           home: BlocBuilder<AuthCubit,AuthState>(
             builder: (context, state){
-              return SignInMob();
+              return SignInMob(name: name,);
             },
           ),
         )
-    )
+    );
   }
 }
 
 
 class SignInMob extends StatelessWidget {
+  String name;
+  SignInMob({Key? key, this.name = "Phone Number"});
 
   TextEditingController phoneController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
         centerTitle: true,
         title: Text("Sign In with Phone"),
       ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
 
-            TextField(
-              controller: phoneController,
-              maxLength: 10,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Phone Number",
-                  counterText: ""
+              TextField(
+                controller: phoneController,
+                maxLength: 10,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    hintText: name,
+                    counterText: ""
+                ),
               ),
-            ),
 
-            SizedBox(height: 10,),
+              const SizedBox(height: 10,),
 
-            BlocConsumer<AuthCubit, AuthState>(
-              listener: (context, state) {
+              BlocConsumer<AuthCubit, AuthState>(
+                listener: (context, state) {
 
-                if(state is AuthCodeSentState) {
-                  Navigator.push(context, CupertinoPageRoute(
-                      builder: (context) => VerifyScreen()
-                  ));
-                }
+                  if(state is AuthCodeSentState) {
+                    Navigator.push(context, CupertinoPageRoute(
+                        builder: (context) => VerifyScreen()
+                    ));
+                  }
 
-              },
-              builder: (context, state) {
+                },
+                builder: (context, state) {
 
-                if(state is AuthLoadingState) {
-                  return Center(
-                    child: CircularProgressIndicator(),
+                  if(state is AuthLoadingState) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  return SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: CupertinoButton(
+                      onPressed: () {
+                        String phoneNumber = "+91" + phoneController.text;
+                        BlocProvider.of<AuthCubit>(context).sendOTP(phoneNumber);
+                      },
+                      color: Colors.blue,
+                      child: const Text("Sign In"),
+                    ),
                   );
-                }
 
-                return SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: CupertinoButton(
-                    onPressed: () {
-                      String phoneNumber = "+91" + phoneController.text;
-                      BlocProvider.of<AuthCubit>(context).sendOTP(phoneNumber);
-                    },
-                    color: Colors.blue,
-                    child: const Text("Sign In"),
-                  ),
-                );
+                },
+              ),
 
-              },
-            ),
-
-          ],
+            ],
+          ),
         ),
       ),
     );
